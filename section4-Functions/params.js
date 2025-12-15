578a0ba43fa1225266e208beae02ecab328ad70d
vigilanteX <dixitaman8@gmail.com>
2025-12-15 16:32:28 +0530

    defaulting params

--- section4-Functions/params.js
+++ section4-Functions/params.js
@@ -2,3 +2,10 @@ function sum(a, b) {
     console.log(a + b);
 }
 sum(5, 10);
+//defaulting params
+function printName(name) {
+    if (name === void 0) { name = "Guest"; }
+    console.log("name is ".concat(name));
+}
+printName();
+printName("Alice");
--- section4-Functions/params.ts
+++ section4-Functions/params.ts
@@ -3,3 +3,11 @@ function sum(a:number, b:number)
     console.log(a + b);
 }
 sum(5, 10);
+
+//defaulting params
+function printName(name:string="Guest")
+{
+    console.log(`name is ${name}`);
+}
+printName();
+printName("Alice");

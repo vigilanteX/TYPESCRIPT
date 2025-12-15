f920243ad297fea5ad1ae5ef29ba4155d3d7ff1f
vigilanteX <dixitaman8@gmail.com>
2025-12-15 16:43:32 +0530

    error case function-specifying return type string but not returning anything as by default return type is void

--- section4-Functions/functionReturnType.js
+++ section4-Functions/functionReturnType.js
@@ -2,3 +2,6 @@ function sum(a, b) {
     return a + b;
 }
 console.log(sum(10, 20));
+function greet(name) {
+    "Hello, ".concat(name, "!");
+}
--- section4-Functions/functionReturnType.ts
+++ section4-Functions/functionReturnType.ts
@@ -3,3 +3,10 @@ function sum(a:number,b:number):number
     return a + b;
 }
 console.log(sum(10,20)); 
+
+
+//A function whose declared type is neither 'undefined', 'void', nor 'any' must return a value.
+// function greet(name:string):string
+// {
+//      `Hello, ${name}!`;
+// }

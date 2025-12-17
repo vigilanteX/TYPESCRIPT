b3b22f9d6262359ceae7b68735edaadb7b835054
vigilanteX <dixitaman8@gmail.com>
2025-12-17 12:03:38 +0530

    readonly

---
+++ section5-Objects/demo05readOnly.js
@@ -0,0 +1,2 @@
+var user1 = { id: 1, username: "john_doe", password: "12345" };
+console.log(user1);
---
+++ section5-Objects/demo05readOnly.ts
@@ -0,0 +1,8 @@
+type User={
+    readonly id:number,
+    readonly username:string,
+    password:string
+}
+
+const user1:User={id:1,username:"john_doe",password:"12345"}
+console.log(user1)

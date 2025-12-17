bae279ec5278718b0c540b16145323dd537247fd
vigilanteX <dixitaman8@gmail.com>
2025-12-17 13:19:21 +0530

    arrays and types

---
+++ section6-Array/demo01ArrayTypes.js
@@ -0,0 +1,10 @@
+var a = [1, 2, 3];
+var b = ['aa', 'ba', 'ca'];
+var c = [4, 5, 6];
+c.push(7);
+c.push(8);
+console.log(c);
+var d = [];
+d.push({ name: 'Aman', age: 18, salary: 3000 });
+d.push({ name: 'Dixit', age: 28, salary: 4000 });
+console.log(d);
---
+++ section6-Array/demo01ArrayTypes.ts
@@ -0,0 +1,23 @@
+const a:number[]=[1,2,3];
+const b:string[]=['aa','ba','ca'];
+
+const c:Array<number>=[4,5,6];
+c.push(7)
+c.push(8)
+console.log(c)
+
+
+type Employee={
+    name:string,
+    age:number,
+    salary:number
+}
+let d:Array<Employee>=[];
+console.log(d);
+d.push({name:'Aman',age:18,salary:3000});
+d.push({name:'Dixit',age:28,salary:4000});
+console.log(d);
+
+
+//2D ARRAY
+const arr:number[][]=[[1,2,3],[4,5,6],[7,8,9]];

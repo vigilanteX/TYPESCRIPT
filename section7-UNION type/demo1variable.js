634262785ed25e7926ccb150ab82758d80ea1cbc
vigilanteX <dixitaman8@gmail.com>
2025-12-18 17:46:26 +0530

    case1-array of different type in which we define type case2-array of similar types in which we specify type

--- section7-UNION type/demo1variable.js
+++ section7-UNION type/demo1variable.js
@@ -15,3 +15,15 @@ function addAge(age) {
 }
 console.log(addAge(25));
 console.log(addAge("25"));
+//either number array or string array or boolean array
+var stuff = [];
+stuff = [1, 2, 3,];
+console.log(stuff);
+stuff = ["a", "b", "c"];
+console.log(stuff);
+stuff = [true, false, true];
+console.log(stuff);
+//stuff2 is an array which can contain number or string or boolean
+var stuff2 = [];
+stuff2 = [1, "a", true, 2, "b", false];
+console.log(stuff2);
--- section7-UNION type/demo1variable.ts
+++ section7-UNION type/demo1variable.ts
@@ -31,3 +31,17 @@ function addAge(age:number | string): number | string
 }
 console.log(addAge(25));
 console.log(addAge("25"));
+
+
+//either number array or string array or boolean array
+let stuff: (number[] | string[] | boolean[])=[];
+stuff=[1,2,3,];
+console.log(stuff);
+stuff=["a","b","c"];
+console.log(stuff);
+stuff=[true,false,true];
+console.log(stuff);
+//stuff2 is an array which can contain number or string or boolean
+let stuff2: (number| string | boolean)[]=[];
+stuff2=[1,"a",true,2,"b",false];
+console.log(stuff2);

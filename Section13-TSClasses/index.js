e9187d6b4a8006b4c804320d7f5068a13e6eea44
vigilanteX <dixitaman8@gmail.com>
2025-12-19 22:56:50 +0530

    assigning value and not providing data type as we have assigned value to it and it can infer type

--- Section13-TSClasses/index.js
+++ Section13-TSClasses/index.js
@@ -1,5 +1,6 @@
 var Player = /** @class */ (function () {
     function Player(first, last) {
+        this.score = 0; //this works as well as we have assigned value to it and it can infer type number
         //we cannot directly this.first=first; without declaring property first
         this.first = first;
         this.last = last;
--- Section13-TSClasses/index.ts
+++ Section13-TSClasses/index.ts
@@ -3,6 +3,7 @@ class Player
     //always property declaration is required in typescript
     first:string;
     last:string;
+    score=0;//this works as well as we have assigned value to it and it can infer type number
     constructor(first:string,last:string)
     {
         //we cannot directly this.first=first; without declaring property first

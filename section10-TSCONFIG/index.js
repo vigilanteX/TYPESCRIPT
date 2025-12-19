e99a472c197db35feed006cdddb98ae8ce5f2098
vigilanteX <dixitaman8@gmail.com>
2025-12-19 12:27:25 +0530

    adding tsconfig and using watch mode to watch for changes in ts and generate js using tsc --watch ./index.ts

---
+++ section10-TSCONFIG/index.js
@@ -0,0 +1,7 @@
+var employee = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30,
+    jobTitle: "Software Engineer",
+    exp: 5
+};
---
+++ section10-TSCONFIG/index.ts
@@ -0,0 +1,14 @@
+interface Employee{
+    firstName: string;
+    lastName: string;
+    age: number;
+    jobTitle: string;
+    exp:number
+}
+const employee: Employee = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30,
+    jobTitle: "Software Engineer",
+    exp: 5
+};

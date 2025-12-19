b8f9b2b22572138fce98d767285090af1ca0f72b
vigilanteX <dixitaman8@gmail.com>
2025-12-19 23:02:08 +0530

    public access modifier

--- Section13-TSClasses/index.ts
+++ Section13-TSClasses/index.ts
@@ -1,9 +1,10 @@
 class Player
 {
     //always property declaration is required in typescript
-    readonly first:string;
-    readonly last:string;
-    score=0;//this works as well as we have assigned value to it and it can infer type number
+    //public is just an access modifier it can be omitted as by default all properties are public and it is just for better readability
+    public readonly first:string;
+    public readonly last:string;
+    public score=0;//this works as well as we have assigned value to it and it can infer type number
     constructor(first:string,last:string)
     {
         //we cannot directly this.first=first; without declaring property first

2f45f58740026d6553efa893b1b764fb527c5f11
vigilanteX <dixitaman8@gmail.com>
2025-12-19 12:37:05 +0530

    specify files in config which will be compiled "files": ["index3.ts"]

--- section10-TSCONFIG/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const employee = {
-    firstName: "John",
-    lastName: "Doe",
-    age: 30,
-    jobTitle: "Software Engineer",
-    exp: 5
-};
---
+++ section10-TSCONFIG/index3.ts
@@ -0,0 +1,7 @@
+type Employee3= {
+    firstName: string;
+    lastName: string;
+    age: number;
+    jobTitle: string;
+    exp: number;
+};

ad85ec3700628b0da487df88a57dae20d405a717
vigilanteX <dixitaman8@gmail.com>
2025-12-22 17:44:27 +0530

    intro to Generics

---
+++ section14-Generics/demo1.js
@@ -0,0 +1,9 @@
+"use strict";
+const nums = [1, 2, 3, 4, 5];
+const nums2 = [7, 8, 9, 0];
+//console.log(nums,nums2)
+function identity(item) {
+    return item;
+}
+const i = identity("aman");
+console.log(i);
---
+++ section14-Generics/demo1.ts
@@ -0,0 +1,10 @@
+const nums:number[]=[1,2,3,4,5];
+const nums2:Array<number>=[7,8,9,0]
+//console.log(nums,nums2)
+function identity<Type>(item:Type):Type
+{
+    return item;
+}
+
+const i=identity<string>("aman")
+console.log(i)

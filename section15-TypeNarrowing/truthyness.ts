daf86f1649f3f4dcd0f16a34b7ce6c9e11135840
vigilanteX <dixitaman8@gmail.com>
2025-12-23 17:22:32 +0530

    Truthyness guard if() check

---
+++ section15-TypeNarrowing/truthyness.ts
@@ -0,0 +1,26 @@
+const element=document.getElementById("meow")
+
+if(element)
+{
+    //getElementById returns null or HTMLElement
+    //in this case we check if element is null or not
+    //and if we are isnide block means element was not null
+    //and if we hover over element we see now only HTMLElement is coming not null
+    //TS is intelligent enough to get type
+    element
+}
+else
+{
+    element
+}
+
+
+let str:string|undefined;
+if(str)
+{
+    str
+}
+else
+{
+    str
+}
